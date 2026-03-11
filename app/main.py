7e863820ccfb45901623b436feef3633d74f77eb
Lucas Sanginetto <lucassanginetto@protonmail.com>
2026-03-11 19:36:32 -0300

    implement user crud endpoints

--- app/main.py
+++ app/main.py
@@ -1,8 +1,96 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel, EmailStr
+from pydantic.dataclasses import dataclass
 
 app = FastAPI()
 
 
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+
+
+class UserIn(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+@dataclass
+class UserRow:
+    id: int
+    username: str
+    email: EmailStr
+    password: str
+
+
+def user_out_from_user_row(row: UserRow) -> UserOut:
+    return UserOut(id=row.id, username=row.username, email=row.email)
+
+
+users_table: list[UserRow] = []
+
+
+@app.get("/users/")
+def get_users() -> list[UserOut]:
+    return list(map(user_out_from_user_row, users_table))
+
+
+@app.get("/users/{id}")
+def get_user(id: int) -> UserOut:
+    def is_requested_user_row(row: UserRow) -> bool:
+        return row.id == id
+
+    try:
+        requested_user_row = next(filter(is_requested_user_row, users_table))
+    except StopIteration:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    return user_out_from_user_row(requested_user_row)
+
+
+@app.post("/users/", status_code=status.HTTP_201_CREATED)
+def post_user(user_in: UserIn) -> UserOut:
+    new_user_row = UserRow(
+        **user_in.model_dump(),
+        id=(1 if len(users_table) == 0 else users_table[-1].id + 1),
+    )
+
+    users_table.append(new_user_row)
+
+    return user_out_from_user_row(new_user_row)
+
+
+@app.put("/users/{id}")
+def put_user(id: int, user_in: UserIn) -> UserOut:
+    def is_requested_user_row(row: UserRow) -> bool:
+        return row.id == id
+
+    try:
+        requested_user_row = next(filter(is_requested_user_row, users_table))
+    except StopIteration:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    new_user_row = UserRow(**user_in.model_dump(), id=id)
+    users_table[users_table.index(requested_user_row)] = new_user_row
+
+    return user_out_from_user_row(new_user_row)
+
+
+@app.delete("/users/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(id: int) -> None:
+    def is_requested_user_row(row: UserRow) -> bool:
+        return row.id == id
+
+    try:
+        requested_user_row = next(filter(is_requested_user_row, users_table))
+    except StopIteration:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    del users_table[users_table.index(requested_user_row)]
+
+
 @app.get("/")
-def get_root():
-    return {"message": "Server is running"}
+def get_root() -> str:
+    return "Server is running"
--- tests/test_app.py
+++ tests/test_app.py
@@ -2,13 +2,104 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
-from app.main import app
 
-
-def test_root_should_return_ok_and_server_running():
-    client = TestClient(app)
-
+def test_get_root(client: TestClient):
     response = client.get("/")
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"message": "Server is running"}
+    assert response.json() == "Server is running"
+
+
+def test_post_user(client: TestClient):
+    response = client.post(
+        "/users/",
+        json={
+            "username": "alice",
+            "email": "alice@test.com",
+            "password": "secret",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "alice",
+        "email": "alice@test.com",
+        "id": 1,
+    }
+
+
+def test_get_users(client: TestClient):
+    response = client.get("/users/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == [
+        {
+            "username": "alice",
+            "email": "alice@test.com",
+            "id": 1,
+        }
+    ]
+
+
+def test_get_user(client: TestClient):
+    response = client.get("/users/1")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "username": "alice",
+        "email": "alice@test.com",
+        "id": 1,
+    }
+
+
+def test_get_user_fail(client: TestClient):
+    response = client.get("/users/2")
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
+
+
+def test_put_user(client: TestClient):
+    response = client.put(
+        "/users/1",
+        json={
+            "username": "bob",
+            "email": "bob@test.com",
+            "password": "newsecret",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "username": "bob",
+        "email": "bob@test.com",
+        "id": 1,
+    }
+
+
+def test_delete_user(client: TestClient):
+    response = client.delete("/users/1")
+
+    assert response.status_code == HTTPStatus.NO_CONTENT
+    assert response.content == b""
+
+
+def test_put_user_fail(client: TestClient):
+    response = client.put(
+        "/users/1",
+        json={
+            "username": "alice",
+            "email": "alice@test.com",
+            "password": "oldsecret",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
+
+
+def test_delete_user_fail(client: TestClient):
+    response = client.delete("/users/1")
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
